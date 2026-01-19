5986f2a75df45d1397c1838aa064432eb2b509e6
Zenous <bennettjoshua712@gmail.com>
2026-01-19 18:51:39 +0530

    Solved Two Sum using two pointers method

---
+++ Two Pointers/167. Two Sum II - Input Array Is Sorted/solution.py
@@ -0,0 +1,16 @@
+def twoSum(numbers,target):
+    length=len(numbers)
+    if length<=1: return False
+    l=0
+    r=length-1
+    while l<=r:
+        print("Hi")
+        if numbers[l]+numbers[r]==target:
+            return [l+1,r+1]
+        elif numbers[l]+numbers[r]>target:
+            r=r-1
+        elif numbers[l]+numbers[r]<target:
+            l=l+1
+    return False
+
+print(twoSum([2,7,11,15],9))
